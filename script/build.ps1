8ace67b55aa4f848dd3eac7987d8c9c7585c52a8
Ryan Svihla <105286284+rsvihladremio@users.noreply.github.com>
2022-07-01 15:21:52 +0200

    audit, bootstrap and build

---
+++ script/build.ps1
@@ -0,0 +1,7 @@
+# script/build: build binary 
+
+# this is also set in script/release and is a copy paste
+$GIT_SHA=@(git rev-parse --short HEAD)
+$VERSION=@(git rev-parse --abbrev-ref HEAD)
+$LDFLAGS="-X github.com/rsvihladremio/ssdownloader/cmd.GitSha=$GIT_SHA -X github.com/rsvihladremio/ssdownloader/cmd.Version=$VERSION"
+go build -ldflags "$LDFLAGS" -o ./bin/ssdownloader.exe
